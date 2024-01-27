cc8433e1297a72767cb042a9480008f84dea9ed9
Josh Guinto <jmpheen@gmail.com>
2024-01-27 21:10:28 +0800

    1/27/2024
    pushed

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -5,12 +5,11 @@ public class PlayerCamera : MonoBehaviour
 {
     PlayerInput playerInput;
     InputAction lookAction;
+    Rigidbody rb;
 
     [Header("Mouse Sensitivity")]
     [SerializeField] float sensX;
     [SerializeField] float sensY;
-
-    Rigidbody rb;
 
     [SerializeField] Transform cameraHolder;
     [SerializeField] Vector3 currentRotation;
@@ -41,16 +40,25 @@ public class PlayerCamera : MonoBehaviour
     {
         Vector3 mouseDelta = lookAction.ReadValue<Vector2>();
 
-        
         currentRotation.x += mouseDelta.x;
+
+        float clampValueX = 180f;
+        if (currentRotation.x > clampValueX)
+        {
+            currentRotation.x -= clampValueX * 2;
+        }
+        if (currentRotation.x < -clampValueX)
+        {
+            currentRotation.x += clampValueX * 2;
+        }
+
         currentRotation.y -= mouseDelta.y;
 
-        float clampValue = 80f;
-        currentRotation.y = Mathf.Clamp(currentRotation.y, -clampValue, clampValue);
+        float clampValueY = 80f;
+        currentRotation.y = Mathf.Clamp(currentRotation.y, -clampValueY, clampValueY);
 
         //vertical
         cameraHolder.rotation = Quaternion.Euler(new Vector3(currentRotation.y, cameraHolder.rotation.eulerAngles.y, 0));
-        Debug.Log(cameraHolder.rotation.eulerAngles.y);
 
         //horizontal
         rb.rotation = Quaternion.Euler(new Vector3(0, currentRotation.x, 0));
